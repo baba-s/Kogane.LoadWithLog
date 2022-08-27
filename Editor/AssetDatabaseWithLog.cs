51b1d5a30492413ae3085491ab50bf3559e5e1ac
baba-s <shotababa1206@gmail.com>
2022-08-27 13:35:19 +0900

    fix

--- Editor/AssetDatabaseWithLog.cs
+++ Editor/AssetDatabaseWithLog.cs
@@ -1,17 +1,24 @@
-﻿using UnityEditor;
+﻿using System;
+using UnityEditor;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace Kogane
 {
     public static class AssetDatabaseWithLog
     {
+        public delegate void OnFailureLoadAssetAtPathDelegate( string assetPath, Type type );
+
+        public static OnFailureLoadAssetAtPathDelegate OnFailureLoadAssetAtPath { get; set; } =
+            ( assetPath, type ) => Debug.LogError( $"{assetPath} に紐づく {type.Name} の AssetDatabase.LoadAssetAtPath に失敗しました" );
+
         public static T LoadAssetAtPath<T>( string assetPath ) where T : Object
         {
             var result = AssetDatabase.LoadAssetAtPath<T>( assetPath );
 
             if ( result == null )
             {
-                Debug.LogWarning( $"{assetPath} に紐づく {typeof( T ).Name} の AssetDatabase.LoadAssetAtPath に失敗しました" );
+                OnFailureLoadAssetAtPath?.Invoke( assetPath, typeof( T ) );
             }
 
             return result;
--- Runtime/ResourcesWithLog.cs
+++ Runtime/ResourcesWithLog.cs
@@ -1,16 +1,23 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace Kogane
 {
     public static class ResourcesWithLog
     {
+        public delegate void OnFailureLoadDelegate( string path, Type type );
+
+        public static OnFailureLoadDelegate OnFailureLoad { get; set; } =
+            ( path, type ) => Debug.LogError( $"{path} に紐づく {type.Name} の Resources.Load に失敗しました" );
+
         public static T Load<T>( string path ) where T : Object
         {
             var result = Resources.Load<T>( path );
 
             if ( result == null )
             {
-                Debug.LogWarning( $"{path} に紐づく {typeof( T ).Name} の Resources.Load に失敗しました" );
+                OnFailureLoad?.Invoke( path, typeof( T ) );
             }
 
             return result;
